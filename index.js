e088533c476f4e2d6265e527dd2cf3411846762a
fiveace-Merill <freddie.ogutu@gmail.com>
2024-11-02 14:23:12 +0300

    final solution commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({dest: './uploads'})
+
 
 var app = express();
 
@@ -11,8 +14,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+	let file = req.file
+	res.json({ name: file.originalname, type: file.mimetype, size: file.size})
+//	upload(req, res, function(err){
+//		if(err instanceof multer.MulterError){
+//			console.log(`${err} -> Multer Error`)
+//		}else if(err){
+//			console.log(`${err} -> Just Error`)
+//		}
+//		console.log(file)
+//	})
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
